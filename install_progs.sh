be03c6c86943c6c266e40b3be949c1f37c854dfe
Matilda Smeds <matilda.smeds@gmail.com>
2019-04-16 11:14:27 +0200

    Add skype and shortcuts

--- install_progs.sh
+++ install_progs.sh
@@ -3,7 +3,7 @@
 # yes Y | sudo apt-get upgrade
 
 
-declare -a progs=('vim' 'gimp' 'keepass2' 'vlc' 'git' 'npm' 'yarn' 'nautilus' 'firefox' 'silversearcher-ag' 'rubocop' 'ruby' 'usb-creator-gtk' 'compiz' 'compizconfig-settings-manager' 'compiz-plugins' 'redis' 'mariadb-client*' 'mariadb-server*')
+declare -a progs=('vim' 'gimp' 'keepassx' 'vlc' 'git' 'npm' 'yarn' 'nautilus' 'firefox' 'silversearcher-ag' 'rubocop' 'ruby' 'usb-creator-gtk' 'redis' 'mariadb-client*' 'mariadb-server*' 'tmux' 'cheese' 'gnome-disk-utility')
 
 for program in "${progs[@]}"
 do
@@ -14,6 +14,11 @@ done
 echo | sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
 yes Y | sudo apt-get install rvm
+
+# Install Skype
+wget https://go.skype.com/skypeforlinux-64.deb -O /tmp/skypeforlinux-64.deb
+yes Y | sudo apt install /tmp/skypeforlinux-64.deb
+rm /tmp/skypeforlinux-64.deb
 
 # Setup java 8 - this might not behave very nicely at all as a script
 # as the installer requires manual steps
@@ -26,9 +31,29 @@ yes Y | sudo apt-get install rvm
 # JRE_HOME=/usr/lib/jvm/java-8-oracle/jre
 # EOL
 
-# Manual steps
+### Manual steps
+## Firefox
 # Firefox: Addons - Privacy Badger, Facebook Container
 # Firefox: Preferences > Privacy & Security - Send "Do Not Track" signal always, Don't save logins
 # Firefox: Preferences > Search: Default search engine DuckDuckGo
 # Connect with Firefox Account
 
+## Skype
+# Dark theme
+
+## Keyboard > Application Shortcuts
+# vlc   - Super+V
+# skype - Super+S
+# xfce4-notes - Super+N
+
+## Window Manager > Keyboard
+# Move window to upper (etc) workspace - Shift+Ctrl+Alt+Arrow
+# Tile window to the top left (etc)    - Super+Alt+Arrow
+
+## Panel > Items
+# Window Menu plugin
+
+# TODO
+#  - Add Put to viewport bindings, perhaps with compiz
+#  - Separate manual steps from actual scripts
+#  - See if bindings could be stored to repo

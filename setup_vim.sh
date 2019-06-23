f572a5d66fa2b78ccba824fc0ec857b8e1793f1c
matildasmeds <matilda.smeds@gmail.com>
2019-06-23 21:33:10 +0300

    Include tmux in vim setup

--- setup_vim.sh
+++ setup_vim.sh
@@ -3,5 +3,6 @@
 cd ~
 mkdir .vim
 cd .vim
-git clone --recurse-submodules https://github.com/matildasmeds/vim_setup.git .
+git clone --recurse-submodules https://github.com/matildasmeds/vim_and_tmux.git .
 ln -s ~/.vim/vimrc ~/.vimrc
+ln -s ~/.vim/.tmux.conf ~/.tmux.conf

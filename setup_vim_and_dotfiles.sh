cec0efa4c476e626e6ec78fe5ffebb38d0f941cf
Matilda Smeds <matildasmeds@users.noreply.github.com>
2019-12-04 21:14:43 +0200

    Update setup_vim_and_dotfiles.sh

--- setup_vim_and_dotfiles.sh
+++ setup_vim_and_dotfiles.sh
@@ -5,5 +5,4 @@ mkdir .vim
 cd .vim
 git clone --recurse-submodules https://github.com/matildasmeds/vim_and_dotfiles.git .
 ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/.tmux.conf ~/.tmux.conf
-ln -s ~/.vim/.zshrc ~/.zshrc
+ln -s ~/.vim/tmux.conf ~/.tmux.conf
